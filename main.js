59326469ab4dcb1239eece45d383e09478d0f192
Jisjo K John <61861646+Jisjo@users.noreply.github.com>
2021-12-18 22:40:18 +0530

    v2

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ http.createServer(function (request, response) {
    // Send the HTTP header 
    // HTTP Status: 200 : OK
    // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.writeHead(200, {'Content-Type': 'text/html'});
    
    // Send the response body as "Hello World"
-   response.end('This is a nodejs -- Hello World -- program\n');
+   response.end('<h1><center>This is a nodejs <i>helow world </i> program</center></h1>');
 }).listen(8081);
 
 // Console will print the message
